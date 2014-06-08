a18f6860ce122c9b5d4f2d1f0fbbecb3be1ae739
coetzeer@gmail.com <coetzeer@gmail.com>
2014-06-08 10:08:28 +0100

    sites.pp added after disaster

---
+++ puppet/manifests/site.pp
@@ -0,0 +1,41 @@
+# create a new run stage to ensure certain modules are included first
+stage { 'pre':
+  before => Stage['main']
+}
+
+# add the baseconfig module to the new 'pre' run stage
+class { 'baseconfig':
+  stage => 'pre'
+}
+
+# all boxes get the base config
+include baseconfig
+
+node 'common' {
+  #include nginx, nginx_vhosts
+}
+
+node 'jira' {
+  #include nginx, nginx_vhosts
+}
+
+node 'crowd' {
+  #include nginx, nginx_vhosts
+}
+
+node 'stash' {
+  #include nginx, nginx_vhosts
+}
+
+node 'fisheye' {
+  #include nginx, nginx_vhosts
+}
+
+node 'confluence' {
+  #include nginx, nginx_vhosts
+}
+
+node 'bamboo' {
+  #include nginx, nginx_vhosts
+}
+
