0bbb76636aa949679dd8e74b889bbc0416a97f51
coetzeer@gmail.com <coetzeer@gmail.com>
2014-07-23 09:02:16 +0100

    update for new version of crucible

--- getBins.sh
+++ getBins.sh
@@ -18,7 +18,9 @@ get_binary atlassian-jira-6.3.1-x64.bin http://www.atlassian.com/software/jira/d
 get_binary atlassian-confluence-5.5.3-x64.bin http://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-5.5.3-x64.bin
 get_binary atlassian-stash-3.1.3-x64.bin http://www.atlassian.com/software/stash/downloads/binary/atlassian-stash-3.1.3-x64.bin
 get_binary atlassian-bamboo-5.5.1.tar.gz http://www.atlassian.com/software/bamboo/downloads/binary/atlassian-bamboo-5.5.1.tar.gz
-get_binary crucible-3.4.5.zip http://www.atlassian.com/software/crucible/downloads/binary/crucible-3.4.5.zip
+#get_binary crucible-3.4.5.zip http://www.atlassian.com/software/crucible/downloads/binary/crucible-3.4.5.zip
+#get_binary fisheye-3.5.0.zip http://downloads.atlassian.com/software/fisheye/downloads/fisheye-3.5.0.zip
+get_binary crucible-3.5.0.zip http://downloads.atlassian.com/software/crucible/downloads/crucible-3.5.0.zip
 get_binary atlassian-crowd-2.7.2.tar.gz http://www.atlassian.com/software/crowd/downloads/binary/atlassian-crowd-2.7.2.tar.gz
 
 cd /vagrant/puppet/binaries
